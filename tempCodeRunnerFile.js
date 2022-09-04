25b8e9d7c2b322d68c2cbe901007d412d98bc314
ebautista07 <estebarile@gmail.com>
2022-09-04 15:57:19 -0500

    cambio puerto

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -6,9 +6,9 @@ const app = express ();
 const publicPath = path.resolve(__dirname, './public')
 app.use (express.static(publicPath))
 
-app.listen (3002, () =>{
-    console.log ("servidor corriendo en el puerto 3002")
+app.listen (3001, () =>{
+    console.log ("servidor corriendo en el puerto 3001")
 });
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'./views/product.html'))
+    res.sendFile(path.resolve(__dirname,'./views/home.html'))
 })
