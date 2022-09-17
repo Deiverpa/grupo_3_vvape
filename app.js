aecc9f20ce55df1492a039f2d70eb89609662eb9
ebautista07 <estebarile@gmail.com>
2022-09-17 14:47:01 -0500

    paginas productos

--- app.js
+++ app.js
@@ -6,12 +6,33 @@ const app = express ();
 const publicPath = path.resolve(__dirname, './public')
 app.use (express.static(publicPath))
 
-app.listen (3005, () =>{
+app.listen (3003, () =>{
     console.log ("servidor corriendo en el puerto 3005")
 });
 
 app.get('/product',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/product.html'))
+})
+app.get('/product2',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/product2.html'))
+})
+app.get('/product3',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/product3.html'))
+})
+app.get('/product4',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/product4.html'))
+})
+app.get('/esencia1',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/esencia1.html'))
+})
+app.get('/esencia2',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/esencia2.html'))
+})
+app.get('/esencia3',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/esencia3.html'))
+})
+app.get('/esencia4',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/esencia4.html'))
 })
 app.get('/productindex',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'./views/productindex.html'))
