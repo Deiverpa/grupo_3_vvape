fec7c2a74dc2ab57c28adcc8e37747469dc9ebbe
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2022-10-10 18:37:02 -0300

    FUNCIONOOOOOOO

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,89 +1,113 @@
+const fs = require("fs");
+const path = require("path");
 
-const fs = require('fs');
-const path = require('path');
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
-const productsFilePath = path.join(__dirname, '../data/products.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
-const mainController={
-    index: (req,res)=>{
-        res.render('index');
-    },
-    register: (req,res)=>{
-        res.render('register');
-    },
-    login: (req,res)=>{
-        res.render('login');
-    },
-    shoppingcart: (req,res)=>{
-        res.render('shoppingcart');
-    },
-    newproduct: (req,res)=>{
-        res.render('newproduct');
-    },
-    modifyproduct: (req,res)=>{
-        let productToEdit = products.find(producto=>producto.id == req.params.id);
-        res.render('modifyproduct', {productToEdit});
-    },
-    update: (req, res) => {
-		let productToUpdate = products.find(producto => producto.id == req.params.id)
-		let image 
-		if(req.files[0] != undefined){
-			image = req.files[0].filename;	
-		}else{
-			image = productToUpdate.image;
-		}
-		let NewProductToUpdate = {
-			id: productToUpdate.id,
-			titulo: req.body.titulo,
-            descripcion: req.body.descripcion,
-            precio: req.body.precio,
-			img: image,
-            stock: req.body.stock,
-            advertencia: "**Producto exclusivo para mayores de edad, pueden contener nicotina, la cual es una sustancia adictiva",
-		}
-		let newProduct = products.map(producto =>{
-			if (producto.id == NewProductToUpdate.id){
-				return producto = {...NewProductToUpdate}
-			} 
-			return producto;
-		})
-        console.log("prueba")
-		fs.writeFileSync(productsFilePath,JSON.stringify(newProduct,null));
-		res.redirect('/')
-	},
-    product: (req,res)=>{
-        let producto = products.find(producto=>producto.id == req.params.id);
-        res.render('product', {producto:producto});
-    },
-    products: (req,res)=>{
-      res.render('products',{products:products});
+const mainController = {
+  index: (req, res) => {
+    res.render("index");
   },
-  store: (req, res) => {
-		let image 
-		if(req.files[0] != undefined){
-			image = req.files[0].filename;	
-		}else{
-			image = 'default-image.jpeg'
-		}
-		let newProduct = {
-			id: products[products.length - 1].id + 1,
+  register: (req, res) => {
+    res.render("register");
+  },
+  login: (req, res) => {
+    res.render("login");
+  },
+  shoppingcart: (req, res) => {
+    res.render("shoppingcart");
+  },
+  newproduct: (req, res) => {
+    res.render("newproduct");
+  },
+  modifyproduct: (req, res) => {
+    let productToEdit = products.find(
+      (producto) => producto.id == req.params.id
+    );
+    res.render("modifyproduct", { productToEdit });
+  },
+  update: (req, res) => {
+    console.log(req.body);
+    let productToUpdate = products.find(
+      (producto) => producto.id == req.params.id
+    );
+    let image;
+    if (req.files[0] != undefined) {
+      image = req.files[0].filename;
+    } else if (productToUpdate.image != undefined) {
+      image = productToUpdate.image;
+    } else {
+      image = "default-image.jpeg";
+    }
+    let NewProductToUpdate = {
+      id: productToUpdate.id,
       titulo: req.body.titulo,
       descripcion: req.body.descripcion,
       precio: req.body.precio,
-			img: image,
-            stock: req.body.stock,
-      advertencia: "**Producto exclusivo para mayores de edad, pueden contener nicotina, la cual es una sustancia adictiva",
+      img: image,
+      /* stock: req.body.stock, */
+      advertencia:
+        "**Producto exclusivo para mayores de edad, pueden contener nicotina, la cual es una sustancia adictiva",
+    };
+    let newProduct = products.map((producto) => {
+      if (producto.id == NewProductToUpdate.id) {
+        return (producto = { ...NewProductToUpdate });
       }
-		products.push(newProduct)
-		fs.writeFileSync(productsFilePath,JSON.stringify(products,null));
-		res.redirect('products')
-	},
-    delete : (req, res) => {
-		let productosFiltrados = products.filter(producto => producto.id != req.params.id)
-		fs.writeFileSync(productsFilePath,JSON.stringify(productosFiltrados,null));
-		res.redirect('products')
-	}
-}
+      return producto;
+    });
+    console.log(newProduct);
+    console.log("prueba2");
+    fs.writeFileSync(productsFilePath, JSON.stringify(newProduct, null));
+    res.redirect("/");
+  },
+  product: (req, res) => {
+    let producto = products.find((producto) => producto.id == req.params.id);
+    res.render("product", { producto: producto });
+  },
+  products: (req, res) => {
+    res.render("products", { products: products });
+  },
+  store: (req, res) => {
+    let image;
+    if (req.files[0] != undefined) {
+      image = req.files[0].filename;
+    } else {
+      image = "default-image.jpeg";
+    }
+    let newProduct = {
+      id: products[products.length - 1].id + 1,
+      titulo: req.body.titulo,
+      descripcion: req.body.descripcion,
+      precio: req.body.precio,
+      img: image,
+      stock: req.body.stock,
+      advertencia:
+        "**Producto exclusivo para mayores de edad, pueden contener nicotina, la cual es una sustancia adictiva",
+    };
+    products.push(newProduct);
+    fs.writeFileSync(productsFilePath, JSON.stringify(products, null));
+    res.redirect("products");
+  },
+  /*   delete: (req, res) => {
+    let productosFiltrados = products.filter(
+      (producto) => producto.id != req.params.id
+    );
+    fs.writeFileSync(
+      productsFilePath,
+      JSON.stringify(productosFiltrados, null)
+    );
+    res.redirect("products");
+  }, */
+  delete: (req, res) => {
+    console.log("HDFOAFJOAKDFA");
+    let id = req.params.id;
+    let finalProducts = products.filter((product) => product.id != id);
+    fs.writeFileSync(
+      productsFilePath,
+      JSON.stringify(finalProducts, null, " ")
+    );
+    res.redirect("/");
+  },
+};
 
 module.exports = mainController;
--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mainController = require('../controllers/mainController')
-const multer = require('multer');
-const path = require('path');
+const mainController = require("../controllers/mainController");
+const multer = require("multer");
+const path = require("path");
 
 let storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        cb(null, 'public/images')
-    },
-    filename: function(req, file, cb){
-        const newFileName = "product" + "_" + Date.now() + path.extname(file.originalname)
-        cb(null,newFileName);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "public/images");
+  },
+  filename: function (req, file, cb) {
+    const newFileName =
+      "product" + "_" + Date.now() + path.extname(file.originalname);
+    cb(null, newFileName);
+  },
+});
 
-let upload = multer({ storage: storage })
+let upload = multer({ storage: storage });
 
 // Principal
-router.get('/', mainController.index)
+router.get("/", mainController.index);
 
 // usuarios
-router.get('/register', mainController.register)
-router.get('/login', mainController.login)
-router.post('/register', mainController.register)
-router.post('/login', mainController.login)
+router.get("/register", mainController.register);
+router.get("/login", mainController.login);
+router.post("/register", mainController.register);
+router.post("/login", mainController.login);
 
 // productos
-router.get('/products', mainController.products)
+router.get("/products", mainController.products);
 // productos detallados
-router.get('/products/:id', mainController.product)
+router.get("/products/:id", mainController.product);
 
 // shoppingK
-router.get('/shoppingcart', mainController.shoppingcart)
+router.get("/shoppingcart", mainController.shoppingcart);
 
 // createproduct
-router.get('/newproduct', mainController.newproduct)
-router.post('/products',upload.any(), mainController.store)
+router.get("/newproduct", mainController.newproduct);
+router.post("/products", upload.any(), mainController.store);
 
 //modifyproduct
-router.get('/products/:id/modifyproduct', mainController.modifyproduct)
-router.put('/products/:id', upload.any(), mainController.update)
+router.get("/products/:id/modifyproduct", mainController.modifyproduct);
+router.post("/products/modify/:id", upload.any(), mainController.update);
 
 // eliminar
-router.delete('/products/:id', mainController.delete); 
+router.post("/products/delete/:id", mainController.delete);
 
 module.exports = router;
